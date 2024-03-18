62b09922cb90aaa4e05360ef98f8412d59a8cc3d
IcyIce_FSDev <iceflier95@gmail.com>
2024-03-17 22:57:36 -0700

    update

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -24,7 +24,14 @@ export default function Breadcrumbs({
               breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
             )}
           >
-            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            <Link
+              href={{
+                pathname: breadcrumb.href,
+                query: { name: breadcrumb.label },
+              }}
+            >
+              {breadcrumb.label}
+            </Link>
             {index < breadcrumbs.length - 1 ? (
               <span className="mx-3 inline-block">/</span>
             ) : null}
